0216a659037af70c22d3cf805f48f35256ca6321
FateNinja <fateninja9376@gmail.com>
2022-11-28 20:59:32 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8001,
-  },
+  // plugins: [react()],
+  base: '/vite-deploy-demo/',
+  // server: {
+  //   port: 8001,
+  // },
 })
