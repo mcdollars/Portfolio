31d6df1cfaa665dc7b7b7029c3bcbfe3049a819e
FateNinja <fateninja9376@gmail.com>
2022-11-29 18:29:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-deploy-demo/',
+  base: '/Portfolio/',
   server: {
     port: 8001,
   },
