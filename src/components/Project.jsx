d78d787feb36b472c3298fc7b58ea2191464350e
FateNinja <fateninja9376@gmail.com>
2022-12-21 13:49:43 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -26,7 +26,7 @@ export default class Project extends Component {
     .then(response => {
       const project_data = response.data;
       this.setState ({projects:project_data.data});
-      console.log(import.meta.env)
+      console.log("datas"+JSON.stringify(import.meta.env))
       console.log("data123"+process.env.key)
     })
   }
